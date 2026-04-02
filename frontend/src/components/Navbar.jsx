13f469ce25e3e906bef7e53b6be4bd6cb4559aa6
Sushilk <sushilkamble7738@gmail.com>
2026-04-02 14:19:01 +0530

    fixed logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,9 +5,10 @@ import { useNavigate } from "react-router-dom";
 
 function Navbar() {
     const navigate = useNavigate();
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
     const username = userInfo?.name;
 
+    
     return(
         <div className="flex justify-between items-center mb-6 px-6 py-4 bg-gray-800 rounded">
             <div 
@@ -27,7 +28,8 @@ function Navbar() {
                 <button 
                     onClick={()=>{
                         localStorage.removeItem("token");
-                        window.location.href = "/login";
+                        localStorage.removeItem("userInfo");
+                        navigate("/login");
                     }}
                     className="px-4 py-2 text-sm bg-blue-500 hover:bg-blue-600 rounded-lg transition"
                 >
