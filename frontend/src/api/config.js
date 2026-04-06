f55778a18e49e79e43bc231595d87523596c2389
Sushilk <sushilkamble7738@gmail.com>
2026-04-06 12:16:37 +0530

    adjusted BASE_URL for environment

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,2 +1,5 @@
-// export const BASE_URL = "https://pulseapi-kbnm.onrender.com";
-export const BASE_URL = "http://127.0.0.1:8000";
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+export const BASE_URL = isLocal
+? "http://127.0.0.1:8000"
+: "https://pulseapi-kbnm.onrender.com";
